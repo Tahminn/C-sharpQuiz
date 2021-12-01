e8b98f22f95dbfe3822944a658201cb7fa21a0fa
Tahmin Fatiyev <tahminshf@code.edu.az>
2021-12-02 00:29:45 +0400

    ValidMail_Factorial_Hierarchy

---
+++ ValidMail/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ValiMail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mail = "cavid@code.edu.az";
+            string pass = "123456";
+
+            CheckingMailandPass("cavid@code.edu.az", "123124135");
+        }
+        static void CheckingMailandPass(string mail, string pass)
+        {
+            if (mail == "cavid@code.edu.az" && pass == "123456")
+            {
+                Console.WriteLine("Signed in");
+            }
+            else
+            {
+                Console.WriteLine("Login or pass is incorrect");
+            }
+        }
+    }
+}
